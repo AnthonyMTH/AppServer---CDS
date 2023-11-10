7151ae721c39b94b4cf4bb0e70b08b0068d6af7c
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2023-11-09 22:27:46 -0500

    Creacion de un primer modelo de usuario simple

---
+++ src/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+//Esquema de los usuarios servira mas adelante
+const userSchema = mongoose.Schema({
+    username: {
+        type: String, //Tipo de dato
+        required: true, //Sirve para que sea requerido
+        trim: true, //Sirve para eliminar espacios
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true, //Sirve para que sea unico y no se repitan emails en registros
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
+export default mongoose.model('User', userSchema); //Crea una coleccion de usuarios y los guarda en objeto, lo hace mongoose
