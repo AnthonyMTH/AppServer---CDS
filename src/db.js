6a66131a4cccfce8f9110d942e5b3748293ee6ef
AnthonyMTH <aticonaha@unsa.edu.pe>
2023-11-10 12:48:17 -0500

    Rutas register y login

--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try{
-        await mongoose.connect("mongodb+srv://Carlos:Pollosal@cluster0.zdfrryr.mongodb.net/?retryWrites=true&w=majority");
-        console.log("DB is connected");
+        await mongoose.connect("mongodb+srv://Carlos:Pollosal@cluster0.zdfrryr.mongodb.net/?retryWrites=true&w=majority")
+        console.log("DB is connected")
     } catch{
-        console.error(err);
+        console.error(err)
     }
 };
