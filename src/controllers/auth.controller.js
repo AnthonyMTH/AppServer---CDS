87f7ce27c1f950ca009c77a9f993dca3f1669a64
AnthonyMTH <aticonaha@unsa.edu.pe>
2023-11-11 14:42:44 -0500

    Corrigiendo register y login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,10 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import createAccessToken from "../libs/jwt.js";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   console.log(req.body);
-  const { email, password, username } = req.body;
+  const { email, password, username, phone, address, photo } = req.body;
 
   try {
     const passwordHash = await bcrypt.hash(password, 10);
@@ -13,6 +13,9 @@ export const register = async (req, res) => {
       username,
       email,
       password: passwordHash,
+      phone,
+      address,
+      photo,
     });
 
     const userSaved = await newUser.save();
@@ -24,39 +27,52 @@ export const register = async (req, res) => {
       id: userSaved._id,
       username: userSaved.username,
       email: userSaved.email,
+      phone: userSaved.phone,
+      address: userSaved.address,
+      photo: userSaved.photo,
       createdAt: userSaved.createdAt,
-      updatedAt: userSaved.updatedAt
+      updatedAt: userSaved.updatedAt,
     });
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
 };
 
 export const login = async (req, res) => {
-    const { email, password } = req.body
+  const { email, password } = req.body;
 
-    try {
+  try {
+    const userFound = await User.findOne({ email });
 
-        const userFound = await User.findOne({email})
+    if (!userFound) return res.status(400).json(["User not found"]);
 
-        if (!userFound) return res.status(400).json(['User not found'])
+    const isMatch = await bcrypt.compare(password, userFound.password);
 
-        const isMatch = await bcrypt.compare(password, userFound.password)  
+    if (!isMatch) return res.status(400).json(["Incorrect password"]);
 
-        if (!isMatch) return res.status(400).json(['Incorrect password'])
+    const token = await createAccessToken({ id: userFound._id });
 
-        const token = await createAccessToken({ id: userFound._id })
-        
-        res.cookie('token', token)      // crea la cookie para la respuesta
+    res.cookie("token", token); // crea la cookie para la respuesta
 
-        res.json({
-            id: userFound._id,
-            username: userFound.username,
-            email: userFound.email,
-            createdAt: userFound.createdAt,
-            updatedAt: userFound.updatedAt
-        })
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-}
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      phone: userFound.phone,
+      address: userFound.address,
+      photo: userFound.photo,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    // vacía el token
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
+};
