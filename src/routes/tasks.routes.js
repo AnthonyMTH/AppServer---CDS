aac3531fe7ba5d992483a2f940e18db206b589dd
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2023-11-12 11:18:47 -0500

    CRUD de los tasks

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { authRequire } from "../middlewares/validatetoken.js";
-
+import {getTasks,getTask,createTask,deleteTask,updateTask} from "../controllers/task.controller.js"
 const router = Router();
 
-router.get('/tasks', authRequire, ( req,res) => res.send('tasks'))
+router.get('/tasks', authRequire,getTasks)
+router.get('/tasks/:id', authRequire,getTask)
+router.post('/tasks', authRequire,createTask)
+router.delete('/tasks/:id', authRequire,deleteTask)
+router.put('/tasks/:id', authRequire,updateTask)
 
 export default router
