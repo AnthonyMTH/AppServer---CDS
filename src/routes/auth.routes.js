89bac2cd569f9194dfc5032e883eb05b75b6e3d4
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2023-11-14 18:27:09 -0500

    Añadiendo verify para proteger rutas en frontend

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register, login, logout, profile} from "../controllers/auth.controller.js"
+import { register, login, logout, profile, verifyToken} from "../controllers/auth.controller.js"
 import { authRequire } from "../middlewares/validatetoken.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
@@ -9,6 +9,7 @@ const router = Router()
 router.post('/register', validateSchema(registerSchema),register)
 router.post('/login', validateSchema(loginSchema), login)
 router.post('/logout', logout)
+router.get('/verify', verifyToken)
 router.get('/profile', authRequire, profile)
 
 export default router
